f2171a31660ffe34997d75eac75b056cb2219805
MW <1176224694@qq.com>
2023-06-26 12:26:01 +0800

    更新的定时器形式的旋转编码器

---
+++ Lib/Hardware/Encoder/EncoderTIM.c
@@ -0,0 +1,66 @@
+#include "EncoderTIM.h"
+
+void Encoder_Init(void) {
+	/* Ê±ÖÓÅäÖÃ */
+	RCC_APB1PeriphClockCmd(Encoder_TIM_Periph, ENABLE);
+	RCC_APB2PeriphClockCmd(Encoder_Periph, ENABLE);
+	/* IO¿ÚÅäÖÃ */
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = Encoder_A_PIN | Encoder_B_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(Encoder_PORT, &GPIO_InitStructure);
+	/* NVICÅäÖÃ */
+	NVIC_InitTypeDef NVIC_InitStruct;
+	NVIC_InitStruct.NVIC_IRQChannel = Encoder_NVIC_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority =1;
+	NVIC_Init(&NVIC_InitStruct);
+	/* ¶¨Ê±Æ÷ÅäÖÃ */
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInitStructure.TIM_Period = 65535;
+	TIM_TimeBaseInitStructure.TIM_Prescaler = 0;
+	TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
+	TIM_TimeBaseInit(Encoder_TIM, &TIM_TimeBaseInitStructure);
+	/* ¶¨Ê±Æ÷ÊäÈë²¶»ñÅäÖÃ */
+	TIM_ICInitTypeDef TIM_ICInitStructure;
+  TIM_ICStructInit(&TIM_ICInitStructure);
+	TIM_ICInitStructure.TIM_Channel = Encoder_A_TIM_Channel | Encoder_B_TIM_Channel;
+	TIM_ICInitStructure.TIM_ICFilter = 0xF;
+	TIM_ICInit(Encoder_TIM, &TIM_ICInitStructure);
+    
+  TIM_EncoderInterfaceConfig(Encoder_TIM, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
+  
+	TIM_SetCounter(Encoder_TIM, 0);
+  TIM_ClearFlag(Encoder_TIM, TIM_FLAG_Update);	
+  TIM_ITConfig(Encoder_TIM,TIM_IT_Update, ENABLE);
+  TIM_Cmd(Encoder_TIM, ENABLE);
+}
+
+int16_t Encoder_GetClear(void) {
+    int16_t count;
+    count = TIM_GetCounter(Encoder_TIM);
+	  TIM_SetCounter(Encoder_TIM, 0);
+    return count;
+}
+
+uint32_t Encoder_Get(void) {
+	uint32_t count = 0;
+	count = Encoder_TIM->CNT;
+	return count;
+}
+
+int16_t overtime = 0;
+void TIM3_IRQHandler(void) {
+	if(TIM_GetITStatus(Encoder_TIM, TIM_IT_Update) == SET) {
+		if(Encoder_TIM->CR1 && TIM_CounterMode_Down != TIM_CounterMode_Down) {
+			overtime++;
+		}	else {
+			overtime--;
+		}
+		TIM_ClearITPendingBit(Encoder_TIM, TIM_IT_Update);
+	}
+}    
---
+++ Lib/Hardware/Encoder/EncoderTIM.h
@@ -0,0 +1,45 @@
+#ifndef __ENCODER_H
+#define __ENCODER_H
+
+///////////////////////////////////////////////////////////
+//
+// 文件功能：定时器的旋转编码器的基础功能
+// 版本：V1.0
+// 作者：墨蔚（MW）
+// 修改时间：2023/05/06
+//
+///////////////////////////////////////////////////////////
+
+#include "stm32f10x.h"
+
+/* 旋转编码器配置信息 */
+#define Encoder_Periph		RCC_APB2Periph_GPIOA		// 总线时钟
+#define Encoder_PORT			GPIOA										// IO端口组
+#define Encoder_A_PIN 		GPIO_Pin_6							// A相IO端口
+#define Encoder_B_PIN 		GPIO_Pin_7							// B相IO端口
+
+#define Encoder_TIM				TIM3					// 定时器
+#define Encoder_TIM_Periph			RCC_APB1Periph_TIM3			// 定时器时钟线
+#define Encoder_NVIC_IRQn 			TIM3_IRQn								// 中断通道
+#define Encoder_A_TIM_Channel		TIM_Channel_1						// 定时器通道
+#define Encoder_B_TIM_Channel		TIM_Channel_2						// 定时器通道
+
+/**
+  * @brief 旋转编码器初始化
+  * @return 无
+  */
+void Encoder_Init(void);
+
+/**
+  * @brief 获取当前旋转编码器的值, 并清零
+  * @return 旋转编码器的值
+  */
+int16_t Encoder_GetClear(void);
+
+/**
+  * @brief 获取当前旋转编码器的值
+  * @return 旋转编码器的值
+  */
+uint32_t Encoder_Get(void);
+
+#endif
