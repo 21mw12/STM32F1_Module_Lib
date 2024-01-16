f38b661a8c7589bd6eac21aa6b680e592afe3f0b
MW <1176224694@qq.com>
2024-01-16 19:10:09 +0800

    更新了硬件SPI的新功能

--- Lib/System/SPI/SPI1Hardware.c
+++ Lib/System/SPI/SPI1Hardware.c
@@ -24,10 +24,33 @@ void  SPI1_Hardware_Init(void) {
 	SPI_InitStructure.SPI_Mode = SPI_Mode_Master;		// SPI主机
 	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;	// 双线全双工
 	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;		// 数据帧为8Bit
-	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;	// 为高位先行
+
+#if SPI1_FirstBit == 1
+	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+#else
+	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_LSB;
+#endif
+
 	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_128;	// 分频系数为128（当前速度为72M/128,分屏越小速度越快）
-	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;		// 空闲状态时钟为第电平
-	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;	// 第一个边沿开始采样
+
+/* 模式3：空闲状态时钟为高电平，第二个边沿开始采样 */
+#if SPI1_Mode == 3
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
+/* 模式2：空闲状态时钟为高电平，第一个边沿开始采样 */
+#elif SPI1_Mode == 2
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+/* 模式1：空闲状态时钟为低电平，第二个边沿开始采样 */
+#elif SPI1_Mode == 1
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
+/* 模式0：空闲状态时钟为低电平，第一个边沿开始采样 */
+#else
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+#endif
+
 	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;			// 软件模拟NSS
 	SPI_InitStructure.SPI_CRCPolynomial = 7;			// CRC的计算数
 	SPI_Init(SPI1, &SPI_InitStructure);
--- Lib/System/SPI/SPISoftware.c
+++ Lib/System/SPI/SPISoftware.c
@@ -52,18 +52,18 @@ void SPI_Software_Init(void) {
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
  	GPIO_Init(SPI_Software_PORT, &GPIO_InitStructure);
 	
-	SPI_CLK_Write(0);
+	SPI_CLK_Write(1);
 }
 
 uint8_t SPI_Software_SwapByte(uint8_t SendByte) {	
 	for (uint8_t i = 0; i < 8; i++) {
 		SPI_MOSI_Write(SendByte & 0x80);
 		SendByte <<= 1;
-		SPI_CLK_Write(1);
+		SPI_CLK_Write(0);
 		if (SPI_MISO_Read() == 1) {
 			SendByte |= 0x01;
 		}
-		SPI_CLK_Write(0);
+		SPI_CLK_Write(1);
 	}
 	
 	return SendByte;
