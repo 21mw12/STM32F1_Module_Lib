11e0de52ef6eff6c63847a5ff29be655db91c185
MW <1176224694@qq.com>
2024-01-30 15:27:49 +0800

    更新了矩阵按键的库

--- Lib/Hardware/MatrixKey/MatrixKey.c
+++ Lib/Hardware/MatrixKey/MatrixKey.c
@@ -2,127 +2,146 @@
 #include "Delay.h"
 
 void MatrixKey_Init(void) {
-	RCC_APB2PeriphClockCmd(MatrixKey_Periph, ENABLE);
+	RCC_APB2PeriphClockCmd(MatrixKey_RPeriph, ENABLE);
+	RCC_APB2PeriphClockCmd(MatrixKey_CPeriph, ENABLE);
 	
 	GPIO_InitTypeDef GPIO_InitStruture;
-	
 	GPIO_InitStruture.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStruture.GPIO_Pin = MatrixKey_Row1 | MatrixKey_Row2 | MatrixKey_Row3 | MatrixKey_Row4;
 	GPIO_InitStruture.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(MatrixKey_Port, &GPIO_InitStruture);
+	GPIO_Init(MatrixKey_RPort, &GPIO_InitStruture);
 	
 	GPIO_InitStruture.GPIO_Mode = GPIO_Mode_IPU;
 	GPIO_InitStruture.GPIO_Pin = MatrixKey_Column1 | MatrixKey_Column2 | MatrixKey_Column3 | MatrixKey_Column4;
-	GPIO_Init(MatrixKey_Port, &GPIO_InitStruture);
+	GPIO_Init(MatrixKey_CPort, &GPIO_InitStruture);
 }
 
-uint8_t MatrixKey(void) {
+/**
+  * @brief 设置所有引脚为高电平
+  * @return 无
+  */
+void MatrixKey_SetAllPin(void){
+	GPIO_SetBits(MatrixKey_RPort, MatrixKey_Row1);
+	GPIO_SetBits(MatrixKey_RPort, MatrixKey_Row2);
+	GPIO_SetBits(MatrixKey_RPort, MatrixKey_Row3);
+	GPIO_SetBits(MatrixKey_RPort, MatrixKey_Row4);
+	GPIO_SetBits(MatrixKey_CPort, MatrixKey_Column1);
+	GPIO_SetBits(MatrixKey_CPort, MatrixKey_Column2);
+	GPIO_SetBits(MatrixKey_CPort, MatrixKey_Column3);
+	GPIO_SetBits(MatrixKey_CPort, MatrixKey_Column4);
+}
+
+uint8_t MatrixKey_GetCode(void) {
 	uint8_t KeyNumber = 0;
 	
-	GPIO_Write(MatrixKey_Port, 0xFF);
-	GPIO_ResetBits(MatrixKey_Port, MatrixKey_Row1);
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column1)) {
+	/* 扫描第一行按键 */
+	MatrixKey_SetAllPin();
+	GPIO_ResetBits(MatrixKey_RPort, MatrixKey_Row1);
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column1)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column1));
+    while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column1));
 		Delay_ms(20);
 		KeyNumber = 1;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column2)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column2)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column2));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column2));
 		Delay_ms(20);
 		KeyNumber = 2;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column3)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column3)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column3));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column3));
 		Delay_ms(20);
 		KeyNumber = 3;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column4)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column4)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column4));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column4));
 		Delay_ms(20);
 		KeyNumber = 4;
 	}
 	
-	GPIO_Write(MatrixKey_Port, 0xFF);
-	GPIO_ResetBits(MatrixKey_Port, MatrixKey_Row2);
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column1)) {
+	/* 扫描第二行按键 */
+	MatrixKey_SetAllPin();
+	GPIO_ResetBits(MatrixKey_RPort, MatrixKey_Row2);
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column1)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column1));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column1));
 		Delay_ms(20);
 		KeyNumber = 5;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column2)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column2)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column2));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column2));
 		Delay_ms(20);
 		KeyNumber = 6;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column3)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column3)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column3));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column3));
 		Delay_ms(20);
 		KeyNumber = 7;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column4)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column4)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column4));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column4));
 		Delay_ms(20);
 		KeyNumber = 8;
 	}
 	
-	GPIO_Write(MatrixKey_Port, 0xFF);
-	GPIO_ResetBits(MatrixKey_Port, MatrixKey_Row3);
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column1)) {
+	/* 扫描第三行按键 */
+	MatrixKey_SetAllPin();
+	GPIO_ResetBits(MatrixKey_RPort, MatrixKey_Row3);
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column1)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column1));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column1));
 		Delay_ms(20);
 		KeyNumber = 9;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column2)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column2)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column2));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column2));
 		Delay_ms(20);
 		KeyNumber = 10;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column3)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column3)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column3));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column3));
 		Delay_ms(20);
 		KeyNumber = 11;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column4)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column4)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column4));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column4));
 		Delay_ms(20);
 		KeyNumber = 12;
 	}
 	
-	GPIO_Write(MatrixKey_Port, 0xFF);
-	GPIO_ResetBits(MatrixKey_Port, MatrixKey_Row4);
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column1)) {
+	/* 扫描第四行按键 */
+	MatrixKey_SetAllPin();
+	GPIO_ResetBits(MatrixKey_RPort, MatrixKey_Row4);
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column1)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column1));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column1));
 		Delay_ms(20);
 		KeyNumber = 13;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column2)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column2)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column2));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column2));
 		Delay_ms(20);
 		KeyNumber = 14;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column3)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column3)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column3));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column3));
 		Delay_ms(20);
 		KeyNumber = 15;
 	}
-	if (!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column4)) {
+	if (!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column4)) {
 		Delay_us(20);
-		while(!GPIO_ReadInputDataBit(MatrixKey_Port, MatrixKey_Column4));
+		while(!GPIO_ReadInputDataBit(MatrixKey_CPort, MatrixKey_Column4));
 		Delay_ms(20);
 		KeyNumber = 16;
 	}
@@ -130,29 +149,39 @@ uint8_t MatrixKey(void) {
 	return KeyNumber;
 }
 
-uint8_t KeyCodeTransition(uint8_t KeyCode) {
+Button MatrixKey_TransitionKeyCode(uint8_t KeyCode) {
 	switch (KeyCode) {
+		case 1:
+			return Button_Num1;
 		case 2:
-			return 0;
+			return Button_Num2;
+		case 3:
+			return Button_Num3;
+		case 4:
+			return Button_A;
 		case 5:
-			return 1;
+			return Button_Num4;
 		case 6:
-			return 2;
+			return Button_Num5;
 		case 7:
-			return 3;
+			return Button_Num6;
+		case 8:
+			return Button_B;
 		case 9:
-			return 4;
+			return Button_Num7;
 		case 10:
-			return 5;
+			return Button_Num8;
 		case 11:
-			return 6;
+			return Button_Num9;
+		case 12:
+			return Button_C;
 		case 13:
-			return 7;
+			return Button_Asterisk;
 		case 14:
-			return 8;
+			return Button_Num0;
 		case 15:
-			return 9;
-		default:
-			return 10;
+			return Button_WellNumber;
+		case 16:
+			return Button_D;
 	}
 }
--- Lib/Hardware/MatrixKey/MatrixKey.h
+++ Lib/Hardware/MatrixKey/MatrixKey.h
@@ -8,21 +8,52 @@
 // 作者：墨蔚（MW）
 // 修改时间：2023/05/06
 //
+// 默认矩阵位置说明：
+//				  C1	C2	C3	C4
+//		R1		1		2		3		A
+//		R2		4		5		6		B
+//		R3		7		8		9		C
+//		R4		*		0		#		D
+//
 ///////////////////////////////////////////////////////////
 
 #include "stm32f10x.h"
 
 /* 矩阵按键引脚配置信息 */
-#define MatrixKey_Periph			RCC_APB2Periph_GPIOB		// 总线时钟
-#define MatrixKey_Port				GPIOB										// IO端口组
-#define MatrixKey_Row1				GPIO_Pin_3							// 矩阵键盘每行的IO端口
-#define MatrixKey_Row2				GPIO_Pin_2
-#define MatrixKey_Row3				GPIO_Pin_1
-#define MatrixKey_Row4				GPIO_Pin_0
-#define MatrixKey_Column1			GPIO_Pin_4							// 矩阵键盘每列的IO端口
-#define MatrixKey_Column2			GPIO_Pin_5
-#define MatrixKey_Column3			GPIO_Pin_6
-#define MatrixKey_Column4			GPIO_Pin_7
+/* 矩阵键盘行的IO端口 */
+#define MatrixKey_RPeriph			RCC_APB2Periph_GPIOB
+#define MatrixKey_RPort				GPIOB
+#define MatrixKey_Row1				GPIO_Pin_12
+#define MatrixKey_Row2				GPIO_Pin_13
+#define MatrixKey_Row3				GPIO_Pin_14
+#define MatrixKey_Row4				GPIO_Pin_15
+/* 矩阵键盘列的IO端口 */
+#define MatrixKey_CPeriph			RCC_APB2Periph_GPIOA
+#define MatrixKey_CPort				GPIOA
+#define MatrixKey_Column1			GPIO_Pin_8
+#define MatrixKey_Column2			GPIO_Pin_9
+#define MatrixKey_Column3			GPIO_Pin_10
+#define MatrixKey_Column4			GPIO_Pin_11
+
+/* 键码值转换 */
+typedef enum {
+	Button_Num0 = 0,
+	Button_Num1,
+	Button_Num2,
+	Button_Num3,
+	Button_Num4,
+	Button_Num5,
+	Button_Num6,
+	Button_Num7,
+	Button_Num8,
+	Button_Num9,
+	Button_Asterisk,		// *
+	Button_WellNumber,	// #
+	Button_A,
+	Button_B,
+	Button_C,
+	Button_D,
+} Button;
 
 /**
   * @brief 矩阵按键初始化
@@ -34,13 +65,23 @@ void MatrixKey_Init(void);
   * @brief 获取矩阵按键建码值
   * @return 无
   */
-uint8_t Get_MatrixKeyCode(void);
+uint8_t MatrixKey_GetCode(void);
 
 /**
   * @brief 将建码值转换为数字（仅适配4*4矩阵键盘中的数字按键部分）
   * @param KeyCode 建码值
   * @return 建码值对应的数字
   */
-uint8_t KeyCode_Transition(uint8_t KeyCode);
+Button MatrixKey_TransitionKeyCode(uint8_t KeyCode);
 
 #endif
+
+///////////////////////////////////////////////////////////
+//
+// 更新日志：
+// V1.0: 2023/05/06
+//				实现了矩阵按键的基础显示
+// V2.0: 2024/01/30
+//				实现了更加通用化的设计
+//
+///////////////////////////////////////////////////////////
